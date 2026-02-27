d6572d32359be63d34fbe400355e95e22d8a03ca
Bendrox <oussamabendrisspro@gmail.com>
2026-02-27 12:00:00 +0100

    chore: document model finished

--- app/modeles/document.py
+++ app/modeles/document.py
@@ -1,2 +1,15 @@
 from ..database import Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
+from datetime import datetime , timezone
+
+class Document(Base):
+    __tablename__="documents"
+    
+    id=Column(Integer, primary_key=True, index=True)
+    filename=Column(String, nullable=False )
+    content=Column(String, nullable=False)
+    status=Column(String, default="PENDING")
+    
+    owner_id= Column(Integer, ForeignKey("users.id"), nullable=False)
+    created_at=Column(DateTime , default=lambda: datetime.now(timezone.utc))
+    
